091b2e9cb3fb4714c825ded42b2616d6d118ddac
Joseantonio920 <71590697+Joseantonio920@users.noreply.github.com>
2020-12-15 17:59:47 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from pygame.locals import *
-pygame.init()
-tela=pygame.display.set_mode((500, 500))
-while True:
-	for event in pygame.event.get():
-		if event.type==QUIT:
-			break
-pygame.quit()
